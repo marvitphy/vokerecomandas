bf186bd3426bbb1f838e47cdcc31212e85ed2c6d
Trincheira - Comunicação Visual <48634870+marvitphy@users.noreply.github.com>
2020-09-15 15:31:01 -0300

    ae

--- api/v1/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $db = new mysqli('localhost', 'u951173166_vc', 'Vcomandas54321', 'u951173166_vc');
-  if ($db->connect_error) {
-    die("Conexão não estabelecida" . $conn->connect_error);
-  }
-
-?>
