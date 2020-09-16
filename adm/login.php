2d6694c072b701862e785da8751e4f289bd8f548
Trincheira - Comunicação Visual <48634870+marvitphy@users.noreply.github.com>
2020-09-16 05:56:09 -0300

    Update login.php

--- adm/login.php
+++ adm/login.php
@@ -35,7 +35,7 @@ if(isset($_POST["pass"])&& isset($_POST["email"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Vokerê Comandas</title>
+    <title>Login - Vokerê</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
