ae7158cc6e63e4c51862fca7b6f2d47b134446bf
Trincheira - Comunicação Visual <48634870+marvitphy@users.noreply.github.com>
2020-09-16 05:39:23 -0300

    Create opcoes.php

---
+++ api/v1/opcoes.php
@@ -0,0 +1,30 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header('Cache-Control: no-cache, must-revalidate');
+header('Pragma: public');
+header('Pragma: no-cache');
+header('Content-type: application/json; charset=utf-8');
+header('Connection: keep-alive');
+header('Access-Control-Allow-Methods: POST');
+header("Access-Control-Allow-Headers: Content-Type, X-Request-With, X-Requested-By");
+
+include 'config.php';
+
+$user_id = $_SESSION['id'];
+
+$sql = "SELECT * FROM especificacoes where user_id = '$user_id'";
+$result = mysqli_query($db, $sql);
+
+$result_json = array();
+$result_json_2 = array();
+
+while ($rows = mysqli_fetch_assoc($result)) {
+  $result_2 = mysqli_query($db, $sql_2);
+  array_push($result_json, $rows);
+
+}
+
+// send the result now
+echo json_encode($result_json);
